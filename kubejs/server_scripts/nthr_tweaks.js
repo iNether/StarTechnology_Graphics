933d1287d1a57a5d07a842f9839325a4ae31c6c3
Oleksandr Komakha <xxADVENTUSxx@gmail.com>
2024-08-17 20:30:35 +0300

    recipe tweaks

---
+++ kubejs/server_scripts/nthr_tweaks.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+    event.shaped(Item.of('chisel_chipped_integration:laboratory_fuzzy_screen'), [
+        'L  ',
+        '   ',
+        '   '
+    ], {
+        'L': 'chisel_chipped_integration:laboratory_white_panel',
+    });
+});
